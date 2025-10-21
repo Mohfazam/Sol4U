1b887482ecfbbb66895374539ce0f1c6239b2cae
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-21 09:05:34 +0530

    init

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+
+export const Navbar = () => {
+  return (
+    <div className="w-full h-11 border border-red-400">
+        <div className="h-full flex justify-center items-center">
+            <a href="/" className="text-2xl font-normal">Sol4U</a>
+            <button className="w-24">Talk To Us</button>
+        </div>
+    </div>
+  )
+}
