3a3ed56c814599062d83175ba7fec63faa3c890e
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-21 09:43:47 +0530

    navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <div className="w-full h-16 border border-red-400 text-black">
+    <div className="w-full h-16 text-black mt-2">
       <div className="h-full flex items-center px-24 relative">
         
         <a href="/" className="text-4xl font-normal lg:absolute left-1/2 -translate-x-1/2 pt-2 pb-2">
@@ -11,7 +11,7 @@ export const Navbar = () => {
         <div className="ml-auto">
           <button
            style={{ fontFamily: "'Pontano Sans', sans-serif" }}
-           className="w-24 border-2 border-[#8075ff] rounded-full p-2 text-[16px] font-Pontano text-[#9d93fe] font-normal flex justify-center items-center hover:bg-[#8075ff] hover:text-white hover:cursor-pointer transition-ease-in-ease-out">
+           className="w-25 border-2 border-[#8075ff] rounded-full p-3 text-[16px] font-Pontano text-[#9d93fe] font-normal flex justify-center items-center hover:bg-[#8075ff] hover:text-white hover:cursor-pointer transition-colors duration-300 ease-in-out">
             <span>Talk To Us</span>
           </button>
         </div>
