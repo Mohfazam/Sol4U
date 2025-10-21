078eac84d1a851230b4339297b44d60e72bad148
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-21 09:40:55 +0530

    Fonts

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,12 +1,21 @@
-
-
 export const Navbar = () => {
   return (
-    <div className="w-full h-11 border border-red-400">
-        <div className="h-full flex justify-center items-center">
-            <a href="/" className="text-2xl font-normal">Sol4U</a>
-            <button className="w-24">Talk To Us</button>
+    <div className="w-full h-16 border border-red-400 text-black">
+      <div className="h-full flex items-center px-24 relative">
+        
+        <a href="/" className="text-4xl font-normal lg:absolute left-1/2 -translate-x-1/2 pt-2 pb-2">
+          Sol4U
+        </a>
+        
+        
+        <div className="ml-auto">
+          <button
+           style={{ fontFamily: "'Pontano Sans', sans-serif" }}
+           className="w-24 border-2 border-[#8075ff] rounded-full p-2 text-[16px] font-Pontano text-[#9d93fe] font-normal flex justify-center items-center hover:bg-[#8075ff] hover:text-white hover:cursor-pointer transition-ease-in-ease-out">
+            <span>Talk To Us</span>
+          </button>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={loosNormal.variable}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Pontano+Sans:wght@300..700&display=swap" rel="stylesheet" />
+      </head>
       <body className="antialiased">
         {children}
       </body>
