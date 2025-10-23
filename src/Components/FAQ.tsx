9db46449eff4c1653973e3b3a3bbb8df9ee7bbb9
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-23 18:20:48 +0530

    Framer in FAQ

--- src/Components/FAQ.tsx
+++ src/Components/FAQ.tsx
@@ -1,13 +1,19 @@
-
+"use client";
+import { motion } from "framer-motion";
 
 const FAQ = () => {
   return (
-    <div className="h-[700px] bg-black flex justify-center items-center">
-      <div className="text-[#8075ff] text-[44px] font-normal leading-[50px]">
+    <div className="h-[700px] bg-black flex justify-center items-center overflow-hidden">
+      <motion.div
+        className="text-[#8075ff] text-[44px] font-normal leading-[50px]"
+        initial={{ y: 0 }}
+        animate={{ y: -250 }}
+        transition={{ delay: 2, duration: 1.5, ease: "easeOut" }}
+      >
         FAQ
-      </div>
+      </motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
