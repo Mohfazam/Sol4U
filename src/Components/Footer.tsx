a8808db3cff468da68220077118995d43f7f901b
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-24 00:08:25 +0530

    Final Touch Ups

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,3 +1,4 @@
+import { Mail, Phone, X } from "lucide-react"
 
 
 const Footer = () => {
@@ -5,14 +6,38 @@ const Footer = () => {
     <div className="bg-[#f9f0fb] h-full w-full p-16">
       <div className="flex flex-col justify-center items-center gap-2">
         <span className="text-[44px] leading-[53px] text-black">Stay in the loop</span>
-        <span className="text-[22px] leading-[26px] font-inter text-black">Subscribe to our newsletter to get our latest updates</span>
+        <span className="text-[22px] leading-[26px] font-inter text-black font-light">Subscribe to our newsletter to get our latest updates</span>
 
-        <div className="w-120 h-20 flex justify-center items-center mt-12 gap-14">
-            <input type="text" className="h-full w-full"/>
-            <button className="h-full bg-[#8075ff] rounded-3xl px-8 flex items-center justify-center text-[14px] leading-[19px] font-inter tracking-[2px]">
-  Subscribe
-</button>
+        <div className="w-134 h-20 flex justify-center items-center mt-12 gap-14">
+          <input type="text" className="h-full w-full border-2 border-[#e0e0e0] text-[#c2c2c2] text-[22px] leading-[26px] tracking-[2px] font-inter text-center rounded" placeholder="Phone Number" />
+          <button className="h-full bg-[#8075ff] rounded-3xl px-9 flex items-center justify-center text-[14px] leading-[19px] font-inter tracking-[2px] cursor-pointer transition-all hover:bg-[#554df7]">
+            SUBSCRIBE
+          </button>
 
+        </div>
+      </div>
+
+      <div className="w-full flex justify-between text-black h-8 py-24">
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-2">
+            <Phone /><span>+91 97045 77468 | +91 9391269124</span>
+          </div>
+
+          <div className="flex gap-2">
+            <Mail /><span>-----------------</span>
+          </div>
+
+          <div className="flex gap-2 w-8 h-8">
+            <img src="/x.svg" alt="coming soon" title="coming soon" />
+            <img src="/instagram.png" alt="coming soon" title="coming soon"/>
+            <img src="/linkedin.svg" alt="coming soon" title="coming soon"/>
+            
+          </div>
+        </div>
+
+
+        <div className="pt-22">
+          Ⓒ 2025 Fortuna. All rights reserved.
         </div>
       </div>
     </div>
