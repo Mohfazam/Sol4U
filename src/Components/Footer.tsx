afd2015759da6b27d29e4482622988b7ef9730e6
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-24 00:41:09 +0530

    Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <div className="bg-[#f9f0fb] h-full w-full p-16">
       <div className="flex flex-col justify-center items-center gap-2">
-        <span className="text-[44px] leading-[53px] text-black">Stay in the loop</span>
-        <span className="text-[22px] leading-[26px] font-inter text-black font-light">Subscribe to our newsletter to get our latest updates</span>
+        <span className="text-[44px] leading-[53px] text-black">Request A Call Back</span>
+        <span className="text-[22px] leading-[26px] font-inter text-black font-light">Our Team Will Reach Out To You Witin 24 Hours</span>
 
-        <div className="w-134 h-20 flex justify-center items-center mt-12 gap-14">
+        <div className="w-144 h-20 flex justify-center items-center mt-12 gap-14">
           <input type="text" className="h-full w-full border-2 border-[#e0e0e0] text-[#c2c2c2] text-[22px] leading-[26px] tracking-[2px] font-inter text-center rounded" placeholder="Phone Number" />
-          <button className="h-full bg-[#8075ff] rounded-3xl px-9 flex items-center justify-center text-[14px] leading-[19px] font-inter tracking-[2px] cursor-pointer transition-all hover:bg-[#554df7]">
-            SUBSCRIBE
+          <button className="h-full bg-[#8075ff] rounded-3xl px-8 flex items-center justify-center text-[14px] leading-[19px] font-inter tracking-[2px] cursor-pointer transition-all hover:bg-[#554df7]">
+            Request Call Back
           </button>
 
         </div>
